366aa4ca944eacf51af307574921eaeb39de78c3
Abdusshh <abdullah.gules@simpplr.com>
2024-08-28 16:44:22 +0300

    Bugfix

--- summarizer/urls.py
+++ summarizer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import summarize_article_view, openai_callback_view, get_csrf_token_view
+from .views import summarize_article_view, openai_callback_view
 
 urlpatterns = [
     path('summarize', summarize_article_view, name='summarize_article'),
