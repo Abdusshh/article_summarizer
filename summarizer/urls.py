f50e54f49b9c444c324ab7f951a0c4f2e23da7b9
Abdusshh <abdullah.gules@simpplr.com>
2024-08-28 15:52:38 +0300

    delete url names

--- summarizer/urls.py
+++ summarizer/urls.py
@@ -3,6 +3,6 @@ from .views import summarize_article_view, send_email, get_csrf_token_view
 
 urlpatterns = [
     path('summarize', summarize_article_view, name='summarize_article'),
-    path('callback', send_email, name='summarization_callback'),  # Callback URL
-    path('get-csrf-token', get_csrf_token_view, name='get_csrf_token'),
+    path('callback', send_email),  # Callback URL
+    path('get-csrf-token', get_csrf_token_view),
 ]
