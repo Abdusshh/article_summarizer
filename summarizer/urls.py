0858e3e3595b4e2c329616a4ace4874ac277e753
Abdusshh <abdullah.gules@simpplr.com>
2024-08-28 16:31:42 +0300

    wrong view added bug

--- summarizer/urls.py
+++ summarizer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import summarize_article_view, send_email, get_csrf_token_view
+from .views import summarize_article_view, openai_callback_view, get_csrf_token_view
 
 urlpatterns = [
     path('summarize', summarize_article_view, name='summarize_article'),
-    path('callback', send_email),  # Callback URL
-    path('get-csrf-token', get_csrf_token_view),
+    path('callback', openai_callback_view, name='summarization_callback'),  # Callback URL
+    path('get-csrf-token', get_csrf_token_view, name='get_csrf_token'),
 ]
